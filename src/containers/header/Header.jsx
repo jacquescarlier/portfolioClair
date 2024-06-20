26976995f03d4c8f7dbd9099a57c45c2a27b5d26
jacquescarlier <jacques.carlier@gmail.com>
2024-06-20 20:35:31 +0200

    change header font color

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,8 +3,8 @@ function Header() {
     <div className="header" id="top">
       <span className="logoRotate">JCR</span>
       <ul className="navigation">
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li className="liNavigation"><a href="#about">About</a></li>
+        <li className="liNavigation"><a href="#contact">Contact</a></li>
       </ul>
     </div>
   );
